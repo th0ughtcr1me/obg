ceafb4e1e834cbb645925fd9feb9db644e8263eb
†нøμ9нτςя¡мə <Th0ughtCr1me@protonmail.ch>
2023-08-12 11:24:26 +0000

    əη$μяə ¢øяяə¢τ ςЯς¡η9

--- src/hashis/mod.rs
+++ src/hashis/mod.rs
@@ -12,9 +12,26 @@ pub const XF_32: Crc<u32> = Crc::<u32>::new(&CRC_32_XFER);
 
 pub fn gcrc128(data: &[u8]) -> [u8; 16] {
     let mut result = [0xff; 16];
-    let mut lhs = hex::decode(format!("{:x}", WE_64.checksum(&data))).unwrap();
+    let mut we64: [u8; 16] = *b"0000000000000000";
+    let prewe64 = format!("{:x}", WE_64.checksum(&data)).as_bytes().to_vec();
+    let modwe64 = prewe64.len() % 16;
+    if modwe64 > 0 {
+        we64[..modwe64].copy_from_slice(&prewe64);
+    } else {
+        we64.copy_from_slice(&prewe64);
+    }
+    let mut go64: [u8; 16] = *b"0000000000000000";
+    let prego64 = format!("{:x}", WE_64.checksum(&data)).as_bytes().to_vec();
+    let modgo64 = prego64.len() % 16;
+    if modgo64 > 0 {
+        go64[..modgo64].copy_from_slice(&prego64);
+    } else {
+        go64.copy_from_slice(&prego64);
+    }
+
+    let mut lhs = hex::decode(we64).expect("failed to CRC64_WE");
     lhs.reverse();
-    let rhs = hex::decode(format!("{:x}", GO_64.checksum(&data))).unwrap();
+    let rhs = hex::decode(go64).expect("failed to CRC64_GO");
     result[..16 / 2].copy_from_slice(&lhs);
     result[16 / 2..].copy_from_slice(&rhs);
     result
@@ -30,7 +47,7 @@ mod gcrc128_tests {
         let input = b"seemingly random bytes";
         assert_equal!(
             gcrc128(input).to_vec(),
-            [185, 59, 126, 244, 2, 252, 171, 21, 102, 82, 177, 220, 251, 137, 169, 191,].to_vec()
+            [185, 59, 126, 244, 2, 252, 171, 21, 21, 171, 252, 2, 244, 126, 59, 185,].to_vec()
         );
     }
 }
@@ -44,14 +61,14 @@ pub fn gcrc256(data: &[u8]) -> [u8; 32] {
     let mut xf32: [u8; 8] = *b"00000000";
 
     let prems64 = format!("{:x}", MS_64.checksum(&data)).as_bytes().to_vec();
-    let modms64 =  prems64.len() % 16;
+    let modms64 = prems64.len() % 16;
     if modms64 > 0 {
         ms64[..modms64].copy_from_slice(&prems64);
     } else {
         ms64.copy_from_slice(&prems64);
     }
     let prejc32 = format!("{:x}", JC_32.checksum(&data)).as_bytes().to_vec();
-    let modjc32 =  prejc32.len() % 8;
+    let modjc32 = prejc32.len() % 8;
     if modjc32 > 0 {
         jc32[..modjc32].copy_from_slice(&prejc32);
     } else {
@@ -111,8 +128,8 @@ mod gcrc256_tests {
         assert_equal!(
             result.to_vec(),
             [
-                191, 169, 137, 251, 220, 177, 82, 102, 21, 171, 252, 2, 244, 126, 59, 185, 179, 0,
-                173, 151, 198, 10, 130, 193, 46, 161, 160, 24, 27, 76, 161, 3
+                185, 59, 126, 244, 2, 252, 171, 21, 21, 171, 252, 2, 244, 126, 59, 185, 179, 0,
+                173, 151, 198, 10, 130, 193, 46, 161, 160, 24, 27, 76, 161, 3,
             ]
             .to_vec()
         );
